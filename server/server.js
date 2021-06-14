305fb09ba55508894cae175595da9df742b5277f
issnoejasso <luisnoejasso@gmail.com>
2021-06-13 21:37:24 -0500

    Connect MongoAtla

--- server/server.js
+++ server/server.js
@@ -1,4 +1,23 @@
 const { GraphQLServer, PubSub } = require("graphql-yoga");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGO_URI = process.env.DB_URL
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Connected to the data Base");
+});
+
+
 
 const messages = [];
 
@@ -40,6 +59,7 @@ const resolvers = {
       subscribers.forEach((fn) => fn());
       return id;
     },
+    
   },
   Subscription: {
     messages: {
